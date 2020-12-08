796fec007defd38b9a3f73af23238b31995d1923
Cedric Wahrig <mail@cedricwahrig.de>
2020-12-08 19:23:57 +0100

    erster Entwurf

--- colorSort.m
+++ colorSort.m
@@ -1,7 +1,14 @@
-function [outputArg1,outputArg2] = colorSort(inputArg1,inputArg2)
-%COLORSORT Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
-end
+function [farbe] = colorSort(r,g,b)
+%COLORSORT Funktion zur Farbzuweisung
+%   Die Funktion erkennt die vom Sensor erfasste Farbe und ordnet sie den
+%   Werten 0=rot, 1=gruen, 2=blau und -1=andereFarbe/Fehler. Die Funktion
+%   erwartet die Werte für r=rot, g=gruen und b=blau.
 
+%Definieren der Variablen
+rot = r;
+gruen = g;
+blau = b;
+
+
+
+end
