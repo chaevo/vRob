e7fc162930b2d648fb78e7f9aecac5495d8e32d5
Cedric Wahrig <mail@cedricwahrig.de>
2020-12-09 18:51:42 +0100

    Zusammenführen der FUnktion

--- colorSort.m
+++ colorSort.m
@@ -1,4 +1,4 @@
-function [farbe] = colorSort(r,g,b)
+function [Farbe] = colorSort(r,g,b)
 %COLORSORT Funktion zur Farbzuweisung
 %   Die Funktion erkennt die vom Sensor erfasste Farbe und ordnet sie den
 %   Werten 0=rot, 1=gruen, 2=blau und -1=andereFarbe/Fehler. Die Funktion
@@ -9,6 +9,19 @@ rot = r;
 gruen = g;
 blau = b;
 
+%Definieren der Farben
+farbeR = 0;
+farbeG = 1;
+farbeB = 2;
 
+%Anlegen einer Matrix zum sortieren nach dem größten Wert
+M = [rot,farbeR;gruen,farbeG;blau,farbeB];
+Ms = sortrows(M);
 
+%Auslesen des größten Farbwerts
+if (Ms(3,1) - Ms(2,1)) > 100;
+    Farbe = Ms(3,2);
+else
+    Farbe = -1;
+end
 end
