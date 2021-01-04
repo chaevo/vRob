71698bed687857a50dc8f9ab740fc51d4c2f5272
Cedric Wahrig <mail@cedricwahrig.de>
2021-01-04 16:58:37 +0100

    if Blöcke geändert und Beschreibung angepasst

--- vector2angle.m
+++ vector2angle.m
@@ -5,14 +5,21 @@ function [angle] = vector2angle(v1,v2)
 %   Falls einer der Vektoren 2 Dimensional ist wird er in einen 3D Vektor
 %   umgeschrieben.
 
+%Umwandlung von Vektor v1 in 3D falls dieser 2D ist
 if (size(v1,2) == 2)
     u1 = [v1(1) v1(2) 0];
+else
+    u1 = v1;
 end
     
+%Umwandlung von Vektor v2 in 3D falls dieser 2D ist
 if (size(v2,2) == 2)
     u2 = [v2(1) v2(2) 0];
+else
+    u2 = v2;
 end
     
+%Berechnung des Schnittwinkels der beiden Vektoren
 angle = atan2d(norm(cross(u1,u2)), dot(u1,u2));
 
 end
