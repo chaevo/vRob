2d8c1ac90dbf03d006bae62dfd9ef72f019a3c20
Beatrice <tricibeck@gmail.com>
2021-01-08 14:07:50 +0100

    Schöner gemacht

--- midPointCircel2.m
+++ midPointCircel2.m
@@ -1,12 +1,13 @@
-function [mid2] = midPointCircel2 (x,z,a)
+function [mid2] = midPointCircel2 (dist,z,h2g)
 % MIDPOINTCIRCEL2 Funktion die ausgehend von der Distanz der
 % Zielkoordinate vom Ursprung die Position der MItte des zweiten Kreises
 % festlegt.
+% a ist der hand2ground Winkel
 
-    x4= sind(a)*129.9
-    y4=cosd(a)*-129.9
+    x4= sind(h2g)*129.9
+    y4=cosd(h2g)*-129.9
 
-mid2= [x,z] - [x4,y4];
+mid2= [dist,z] - [x4,y4];
 
  
 end
--- schnittpunkt.m
+++ schnittpunkt.m
@@ -1,10 +1,15 @@
-function [schnitt] = schnittpunkt (A,B,a,b)
+function [schnitt] = schnittpunkt (mid2)
 % SCHNITTPUNKT Berechnung des Schnittpunkts zweier Kreise mit Eingabe von
 % Radius eines Kreises a,b und den Mittelpunkt A,B
 % Der Schnittpunkt mit dem größeren y wird ausgegeben.
 
+% Variablen
+a= 166
+b=218
+A= [0 122]
 
-c = sqrt((B(1)-A(1))^2+(B(2)-A(2))^2);
+
+c = sqrt((mid2(1)-A(1))^2+(mid2(2)-A(2))^2);
 
 
 x= (a^2+c^2-b^2)/(2*c);
@@ -13,12 +18,12 @@ x= (a^2+c^2-b^2)/(2*c);
 y= sqrt(a^2-x^2);
 
 % Berechnung Schnittunkt 1
-Q1x = A(1) + x*((B(1)-A(1))/c)-y*((B(2)-A(2))/c);
-Q1y = A(2) + x*((B(2)-A(2))/c)+y*((B(1)-A(1))/c);
+Q1x = A(1) + x*((mid2(1)-A(1))/c)-y*((mid2(2)-A(2))/c);
+Q1y = A(2) + x*((mid2(2)-A(2))/c)+y*((mid2(1)-A(1))/c);
 
 % Berechnung Schnittunkt 2
-Q2x = A(1) + x*((B(1)-A(1))/c)+y*((B(2)-A(2))/c);
-Q2y = A(2) + x*((B(2)-A(2))/c)-y*((B(1)-A(1))/c);
+Q2x = A(1) + x*((mid2(1)-A(1))/c)+y*((mid2(2)-A(2))/c);
+Q2y = A(2) + x*((mid2(2)-A(2))/c)-y*((mid2(1)-A(1))/c);
 
 % Vergleich der Punkte
 
