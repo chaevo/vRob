e82367f8b360296db75e8937ba62657201941141
Beatrice <tricibeck@gmail.com>
2021-01-08 12:52:06 +0100

    no message

--- midPointCircel2.m
+++ midPointCircel2.m
@@ -3,8 +3,8 @@ function [mid2] = midPointCircel2 (x,z,a)
 % Zielkoordinate vom Ursprung die Position der MItte des zweiten Kreises
 % festlegt.
 
-    x4= sind(a)*129.9;
-    y4=cosd(a)*-129.9;
+    x4= sind(a)*129.9
+    y4=cosd(a)*-129.9
 
 mid2= [x,z] - [x4,y4];
 
