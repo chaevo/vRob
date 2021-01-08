e5589b44d463e07fc5bce77d0888072bc72f18a7
Beatrice <tricibeck@gmail.com>
2021-01-08 13:23:04 +0100

    falsch

--- schnittpunkt.m
+++ schnittpunkt.m
@@ -12,17 +12,16 @@ y= sqrt(a^2-x^2);
 
 
 
-Q1x = A(1) + x*((B(1)-A(1))/c)-y*((B(1)-A(1))/c)
-Q1y = A(2) + x*((B(2)-A(2))/c)+y*((B(2)-A(2))/c)
+Q1x = A(1) + x*((B(1)-A(1))/c)
+% -y*((B(1)-A(1))/c)
+Q1y = A(2)+ y*((B(2)-A(2))/c)
+
+% + x*((B(2)-A(2))/c)+
+ Q2x = A(1) + x*((B(1)-A(1))/c)+y*((B(1)-A(1))/c)
+ Q2y = A(2) + x*((B(2)-A(2))/c)-y*((B(2)-A(2))/c)
 
 
-Q2x = A(1) + x*(B(1)-A(1)/c)+y*(B(1)-A(1)/c)
-Q2y = A(2) + x*(B(2)-A(2)/c)-y*(B(2)-A(2)/c)
-
-Q1x = round(Q1x)
-Q1y = round(Q1y)
-
-if Q1y > Q2y
+if (Q1y > Q2y)
     schnitt = [Q1x Q1y]
 else
     schnitt = [Q2x Q2y]
