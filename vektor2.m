dca63a3fc600ba68d40b0ec2768f06951b1846ce
Janni <j.mandis@gmx.de>
2021-01-08 14:18:30 +0100

    Vektor-Funktionen

---
+++ vektor2.m
@@ -0,0 +1,5 @@
+function [v2] = vektor2(schnitt)
+% Hier wird der zweite Vektor definiert
+A = [0,122]
+
+v2 = schnitt - A
---
+++ vektor3.m
@@ -0,0 +1,5 @@
+function [v3] = vektor3(schnitt,mid2)
+% Hier wird der dritte Vektor definiert
+
+
+v3 = mid2 - schnitt
